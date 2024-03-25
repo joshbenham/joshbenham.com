4f2bd68f962ec0d3fda5aad3873caf07d6e30bc6
Josh Benham <joshbenham@gmail.com>
2024-03-25 22:08:20 +1100

    Added the Filament Package and renamed welcome to home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -164,6 +164,8 @@
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+
+                        <a href="/admin">Admin</a>
                     </footer>
                 </div>
             </div>
