6e11db6bb521332e832e967d92bc10df7c01cfb0
Josh Benham <joshbenham@gmail.com>
2024-04-29 18:09:23 +1000

    Added Spatie google font package

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,10 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>{{ $title ?? 'joshbenham.com' }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        @googlefonts
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
