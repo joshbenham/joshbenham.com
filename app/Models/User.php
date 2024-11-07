4030c524b54f22c1d9d8537422e127836f26e09b
Josh Benham <joshbenham@gmail.com>
2024-11-07 15:42:49 +1100

    Updated files to be inline with latest Laravel

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements FilamentUser
 {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,16 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 export default {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
     ],
     theme: {
         extend: {
             fontFamily: {
-                'sans': ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             }
         },
     },
